fd3b3d3c37e9a4e15d61af9bafd527ffd4541a25
darcyj <jack.l.darcy@gmail.com>
2019-09-05 13:43:00 -0600

    05sept2019

--- occupancy_sensitivity/phy_occupancy_sensitivity.r
+++ occupancy_sensitivity/phy_occupancy_sensitivity.r
@@ -4,12 +4,9 @@
 	#load data
 	set.seed(12345)
 	hostre <- rtree(200)
-	hosvar <- rep(hostre$tip.label, length=g_nsamp)
+	hosvar <- rep(hostre$tip.label, length=500)
 	#hosidl <- sample(hostre$tip.label, 1)
 	hosidl <- "t85"
-
-	hosvar <- endophyte$metadata$PlantGenus
-	hostre <- endophyte$supertree
 	source("sliding_window.r")
 
 	# visualize distribution of patristic distances
@@ -23,7 +20,7 @@
 		# 3. up = 0.40
 	# each will be analyzed both weighted and unweighted.
 	# define factors common to all 3 sims:
-	prm <- list( sdev=70, ideal=hosidl, hosts=hosvar, hosts_phylo=hostre,
+	prm <- list( sdev=3, ideal=hosidl, hosts=hosvar, hosts_phylo=hostre,
 		n_obs = rep(5:1250, length.out=500), n_cores=12, seed = 1234567, n_sim = 300)
 
 	# simulate matrices
@@ -70,21 +67,3 @@
 	plotsrow(0.3, sim_up30, spec_up30)
 	plotsrow(0.6, sim_up60, spec_up60)
 	dev.off()
-
-	plotsrow <- function(up, sim, spec, ylm=ylimits){
-		# 1. probability distribution for up
-		plot_combined_dist_vec(up, prm$sdev, prm$ideal, envvar, ylab=paste("P(s|env). up =", up), xlab="Elevation")
-		# 2. effect of occupancy
-		occ <- colSums(sim$matrix > 0)
-		lm_occ <- lm(spec$SES ~ occ)
-		plot(spec$SES ~ occ, pch=20, xlab="occupancy", ylab="SES", ylim=ylm)
-		#abline(lm_occ, col="red", lwd=2)
-		# 3. SES sensitivity to n_obs
-		plot(spec$SES ~ colSums(sim$matrix), pch=20, xlab="# observations", ylab="SES", ylim=ylm)
-		# 4 effect of shannon
-		#shan <- apply(X=sim$matrix, MAR=2, FUN=shannon)
-		#plot(spec$SES ~ shan, pch=20, xlab="Shannon", ylab="SES", ylim=ylm)
-		# 4. variance
-		#vdat <- sliding_window(colSums(sim$matrix), lm_occ$residuals, var, 30)
-		#plot(var ~ x_mean, data=vdat, type="l", col="red", lwd=2, xlab="# observations", ylab="var(specificity)")
-	}
