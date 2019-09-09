49da3eba5daa7820f239d111547b19575c3129cd
darcyj <jack.l.darcy@gmail.com>
2019-09-09 11:19:46 -0600

    09sept2019

--- scaling_sensitivity/scaling_sensitivity.r
+++ scaling_sensitivity/scaling_sensitivity.r
@@ -6,10 +6,9 @@
 	envvar <- seq(from=0, to=1000, length.out=500)
 	sim <- env_spec_sim( sdev=200, ideal=600, env=envvar, n_obs=10000, 
 			n_cores=3, up=0, oceanp=0.25)
-plot(sim$matrix[,1] ~ envvar)
 
 ## scale envvar by 1 to 100 and calculate SES
-	scales <- 1:100
+	scales <- (1:25) * 4
 	pval <- ses <- rep(0, length(scales))
 	for(i in 1:length(scales)){
 		envvar_s <- envvar * scales[i]
@@ -29,4 +28,27 @@ plot(sim$matrix[,1] ~ envvar)
 	plot(pval ~ scales, pch=20, xlab="Scaling multiplier", ylab="P-value")
 	dev.off()
 
-	
+
+## add intercept to envvar by 1 to 100 and calculate SES
+	ints <- (1:25) * 4
+	pval2 <- ses2 <- rep(0, length(ints))
+	for(i in 1:length(ints)){
+		envvar_s <- envvar * ints[i]
+		res_s <-  phy_or_env_spec(sim$matrix, env=envvar_s, n_cores=3, n_sim=1000)
+		ses2[i] <- res_s$SES[1]
+		pval2[i] <- res_s$Pval[1]
+	}
+
+## found tiny tiny difference due to rounding errors, so round everything to 4 decimal places
+	pval2 <- round(pval2, 4)
+	ses2 <- round(ses2, 4)
+
+## make a plot
+	pdf("intercept_sens.pdf")
+	par(mfrow=c(2,1))
+	plot(ses2 ~ ints, pch=20, xlab="Intercept Adder", ylab="specificity (SES)")
+	plot(pval2 ~ ints, pch=20, xlab="Intercept Adder", ylab="P-value")
+	dev.off()
+
+## save workspace
+	save(list=ls(), file="stuff.rdata")
